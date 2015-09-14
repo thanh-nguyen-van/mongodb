6c3fa4c89cd1b64d5a9aca138a56e8561eedde0d
thanh <thanh.nguyen2891@gmail.com>
2015-09-14 21:18:42 +0700

    hash password

--- user.js
+++ user.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
 
 var UserSchema = new Schema({
 	name: String,
@@ -9,6 +10,18 @@ var UserSchema = new Schema({
 
 });
 
+UserSchema.pre('save', function(next) {
+
+	var user = this;
+	if(!user.isModified('password')) return next();
+	bcrypt.hash(user.password, null, null function(err, hash)) {
+		if(err) return next(err);
+		user.password = hash;
+ 		next();
+
+	});
+	
+})
 module.exports = mongoose.model('User',UserSchema);
 
 
