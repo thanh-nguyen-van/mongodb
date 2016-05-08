7451bc1eb3c87972d614a3c9b1fa7d7b6a48c6a6
thanh <thanh.nguyen2891@gmail.com>
2016-05-08 17:24:51 +0700

    test

---
+++ node/app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+///////////////
+//app.set("views",__dirname);
+//app.set('view engine', 'ejs');
+//var routes = require('./routes.js');
+
+//app.get("/:name", function(req,res){
+//
+//	res.render("template", { name: req.params.name});
+//});
+app.get('/', routes.sayHello)
+
+app.listen(3000);
+console.log("Seriver running ....");
+
